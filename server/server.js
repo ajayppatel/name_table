8951698dfc69c38b9c1c7b6a4640edf5b302970f
ajayppatel <app2xr@virginia.edu>
2020-07-08 01:42:05 -0400

    Removed code duplication and addressed variable scoping issues

--- server/server.js
+++ server/server.js
@@ -5,34 +5,30 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json({type: 'application/json'}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var dbNames = [];
 
 app.get('/', function(req, res) {
-	var resNames = {
-		names: dbNames
-	};
-	res.status(200).send(JSON.stringify(resNames));
-	console.log("Response " + JSON.stringify(resNames));
+  var resNames = {
+    names: dbNames
+  };
+  res.status(200).send(JSON.stringify(resNames));
+  console.log("Response " + JSON.stringify(resNames));
 });
 
 app.post('/', function (req, res) {
   console.log("Request body: " + JSON.stringify(req.body));
   var name = req.body.name;
   console.log("Name: " + name);
-  names.push(name);
-  console.log("Names: " + JSON.stringify(names));
+  dbNames.push(name);
+  console.log("Names: " + JSON.stringify(dbNames));
   res.status(200).send();
 });
 
-app.post('/data', function (req, res) {
-  console.log("Request body: " + JSON.stringify(req.body));
-  var name = req.body.name;
-  console.log("Name: " + name);
-  names.push(name);
-  console.log("Names: " + JSON.stringify(names));
-  res.status(200).send();
-});
+app.post('/test', function (req, res) {
+  res.send('POST request to the homepage')
+})
 
 if (module === require.main) {
   // [START server]
